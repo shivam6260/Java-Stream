7ddfd2513f02f9c2f5c6376af0a4e3cf8b149e29
Shivam Shrivastav <shivam23711@gmail.com>
2025-05-25 13:50:09 +0530

    Learning Java Stream

---
+++ src/test/java/com/streams/JavaStreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
